47f7dc4f474a4cc95899b1aec54795dedcb57d66
samvit <samvit@hotmail.com>
2021-06-04 22:36:48 +0530

    added matic to the tracker for the lols

--- tracker.py
+++ tracker.py
@@ -16,11 +16,77 @@ def get_price_dcx():
     price_dcx = (data[1]["p"])
     return price_dcx
 
+def get_price_matic_dcx():
+    url = "https://public.coindcx.com/market_data/trade_history?pair=I-MATIC_INR&limit=50"
+    response = requests.get(url)
+    data = response.json()
+    price_dcx = (data[1]["p"])
+    return price_dcx
 
 def get_price_bns():
     usdt = bitbnsObj.getTickerApi('USDT')
     price_bitbns = (usdt['USDT']['last_traded_price'])
     return price_bitbns
+
+def get_price_matic_bns():
+    usdt = bitbnsObj.getTickerApi('MATIC')
+    price_bitbns = (usdt['MATIC']['last_traded_price'])
+    return price_bitbns
+
+def spread_usdt():
+    pricebns = get_price_bns()
+    pricedcx = get_price_dcx()
+    if pricedcx > pricebns:
+        spread = ((pricedcx - pricebns) / pricebns) * 100
+    else:
+        spread = ((pricebns - pricedcx) / pricedcx) * 100
+
+    return spread
+
+def spread_matic():
+    pricebns = get_price_matic_bns()
+    pricedcx = get_price_matic_dcx()
+    if pricedcx > pricebns:
+        spread = ((pricedcx - pricebns) / pricebns) * 100
+    else:
+        spread = ((pricebns - pricedcx) / pricedcx) * 100
+
+    return spread
+
+def inform(c,difference):
+    if c == "USDT":
+        pricebns = get_price_bns()
+        pricedcx = get_price_dcx()
+        spread = spread_usdt()
+    else:
+        pricebns = get_price_matic_bns()
+        pricedcx = get_price_matic_dcx()
+        spread = spread_matic()
+
+    if spread > difference:
+        if pricedcx > pricebns:
+            send_message(
+                chat_id=1340763512,
+                msg=f'{c} spread is {spread} Go buy in Bitbns',
+                bot_token='1868431736:AAFE410MUEsn1hfbR6PB1LT6QiF_A2l1280')
+            send_message(
+                chat_id=1200858304,
+                msg=f'{c} spread is {spread} Go buy in Bitbns',
+                bot_token='1816688031:AAFHkK0ZffO9BXtd-nS6NaCL6UP3SV_JSZs')
+            return 60
+        else:
+            send_message(
+                chat_id=1340763512,
+                msg=f'{c} spread is {spread} Go buy in Coindcx',
+                bot_token='1868431736:AAFE410MUEsn1hfbR6PB1LT6QiF_A2l1280')
+            send_message(
+                chat_id=1200858304,
+                msg=f'{c} spread is {spread} Go buy in Coindcx',
+                bot_token='1816688031:AAFHkK0ZffO9BXtd-nS6NaCL6UP3SV_JSZs')
+            return 60
+    return 3 * 60
+
+
 
 
 def send_message(chat_id, msg, bot_token):
@@ -29,13 +95,6 @@ def send_message(chat_id, msg, bot_token):
     requests.get(url)
 
 
-def wazir():
-    dict1 = {}
-    url =  'https://api.wazirx.com/api/v2/tickers'
-    response = requests.get(url)
-    data2 = response.json()
-    return (data2['usdtinr']['last'])
-
 def main():
     spread_list = []
 
@@ -43,55 +102,31 @@ def main():
 
         pricebns = get_price_bns()
         pricedcx = get_price_dcx()
-        pricewazir = float(wazir())
 
-        if pricewazir < 76.2:
-            send_message(
-                chat_id=1340763512,
-                msg=f'USDT price in wazir is {pricewazir}',
-                bot_token='1868431736:AAFE410MUEsn1hfbR6PB1LT6QiF_A2l1280')
+        matic_dcx = get_price_matic_dcx()
+        matic_bns = get_price_matic_bns()
+
+        time_interval = 180
         print(pricebns, pricedcx)
-        if pricedcx > pricebns:
-            spread = ((pricedcx - pricebns) / pricebns) * 100
-        else:
-            spread = ((pricebns - pricedcx) / pricedcx) * 100
-
+        spread = spread_usdt()
         spread_list.append(spread)
         print(spread)
-        time_interval = 60 * 3
-        # if spread > 0.6:
-        #     time_interval = 60
-        #     if pricedcx > pricebns:
-        #         send_message(
-        #             chat_id=1340763512,
-        #             msg=f'USDT spread is {spread} Go buy in Bitbns',
-        #             bot_token='1868431736:AAFE410MUEsn1hfbR6PB1LT6QiF_A2l1280')
-        #         send_message(
-        #             chat_id=1200858304,
-        #             msg=f'USDT spread is {spread} Go buy in Bitbns',
-        #             bot_token='1816688031:AAFHkK0ZffO9BXtd-nS6NaCL6UP3SV_JSZs')
-        #     else:
-        #         send_message(
-        #             chat_id=1340763512,
-        #             msg=f'USDT spread is {spread} Go buy in Bitdcx',
-        #             bot_token='1868431736:AAFE410MUEsn1hfbR6PB1LT6QiF_A2l1280')
-        #         send_message(
-        #             chat_id=1200858304,
-        #             msg=f'USDT spread is {spread} Go buy in Bitdcx',
-        #             bot_token='1816688031:AAFHkK0ZffO9BXtd-nS6NaCL6UP3SV_JSZs')
-        # else:
-        #     time_interval = 3*60
-        # if len(spread_list) > 20:
-        #     send_message(
-        #         chat_id=1340763512,
-        #         msg=spread_list,
-        #         bot_token='1868431736:AAFE410MUEsn1hfbR6PB1LT6QiF_A2l1280')
-        #     send_message(
-        #         chat_id=1200858304,
-        #         msg=spread_list,
-        #         bot_token='1816688031:AAFHkK0ZffO9BXtd-nS6NaCL6UP3SV_JSZs')
-        #     spread_list = []
-            # empty the price_list
+        x = inform("USDT", 0.6)
+        x = inform("MATIC", 3.5)
+        spread2 = spread_matic()
+
+
+        if len(spread_list) > 20:
+            send_message(
+                chat_id=1340763512,
+                msg=spread_list,
+                bot_token='1868431736:AAFE410MUEsn1hfbR6PB1LT6QiF_A2l1280')
+            send_message(
+                chat_id=1200858304,
+                msg=spread_list,
+                bot_token='1816688031:AAFHkK0ZffO9BXtd-nS6NaCL6UP3SV_JSZs')
+            spread_list = []
+
 
         time.sleep(time_interval)
 
