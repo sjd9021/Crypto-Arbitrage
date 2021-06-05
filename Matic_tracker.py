6c83110b1c182859e394d7f24140ac77d6ba99e8
samvit <samvit@hotmail.com>
2021-06-05 14:30:12 +0530

    bored

--- Matic_tracker.py
+++ Matic_tracker.py
@@ -9,22 +9,24 @@ bitbnsObj = bitbns(key, secretKey)
 threshold = 30000
 
 
-def get_price_dcx():
-    url = "https://public.coindcx.com/market_data/trade_history?pair=I-USDT_INR&limit=50"
+def get_price_matic_bns():
+    usdt = bitbnsObj.getTickerApi('XRP')
+    price_bitbns = (usdt['XRP']['last_traded_price'])
+    return price_bitbns
+
+
+def get_price_matic_dcx():
+    url = "https://public.coindcx.com/market_data/trade_history?pair=I-MATIC_INR&limit=50"
     response = requests.get(url)
     data = response.json()
     price_dcx = (data[1]["p"])
     return price_dcx
 
 
-def get_price_bns():
-    usdt = bitbnsObj.getTickerApi('USDT')
-    price_bitbns = (usdt['USDT']['last_traded_price'])
-    return price_bitbns
-
-def spread_usdt():
-    pricebns = get_price_bns()
-    pricedcx = get_price_dcx()
+def spread_matic():
+    pricebns = get_price_matic_bns() * 1.0025
+    pricedcx = get_price_matic_dcx() * 0.9999
+    spread = 0
     if pricedcx > pricebns:
         spread = ((pricedcx - pricebns) / pricebns) * 100
     else:
@@ -33,12 +35,10 @@ def spread_usdt():
     return spread
 
 
-
-def inform(c,difference):
-    pricebns = get_price_bns()
-    pricedcx = get_price_dcx()
-    spread = spread_usdt()
-
+def inform(c, difference):
+    pricebns = get_price_matic_bns()
+    pricedcx = get_price_matic_dcx()
+    spread = spread_matic()
 
     if spread > difference:
         if pricedcx > pricebns:
@@ -64,11 +64,9 @@ def inform(c,difference):
     return 3 * 60
 
 
-
-
 def send_message(chat_id, msg, bot_token):
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={msg}"
-# send the msg
+    # send the msg
     requests.get(url)
 
 
@@ -77,16 +75,15 @@ def main():
 
     while True:
 
-        pricebns = get_price_bns()
-        pricedcx = get_price_dcx()
-
+        matic_dcx = get_price_matic_dcx()
+        matic_bns = get_price_matic_bns()
 
         time_interval = 180
-        print(pricebns, pricedcx)
-        spread = spread_usdt()
+        print(matic_bns, matic_dcx)
+        spread = spread_matic()
         spread_list.append(spread)
         print(spread)
-        x = inform("USDT", 0.6)
+        x = inform("MATIC", 3)
 
         if len(spread_list) > 20:
             send_message(
@@ -99,9 +96,7 @@ def main():
                 bot_token='1816688031:AAFHkK0ZffO9BXtd-nS6NaCL6UP3SV_JSZs')
             spread_list = []
 
-
         time.sleep(time_interval)
 
 
 main()
-
